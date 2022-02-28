be71454de6616d065f68692da76aff1038d746cb
wilcer1 <wille.cervin@live.se>
2022-02-28 21:27:38 +0100

    eqw

---
+++ validate.js
@@ -0,0 +1,29 @@
+
+
+function validateinput(){
+    const quest1 = new question1("How many letters are there in 'Hello' ?", [1, 2, 5], 2);
+    alert("hello")
+}
+
+
+alert("hello")
+
+class question {
+    constructor(question, answers, right_answer_idx) {
+        this.question = question;
+        this.answers = answers;
+        this.right_answer_idx = right_answer_idx;
+    }
+  }
+class question1 extends question {
+    super()
+
+}
+class question2 extends question {
+    super()
+
+}
+class question3 extends question {
+    super()
+
+}
