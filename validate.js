72fb4321d1170fad4203a9d01b6fe24c0c266d87
wilcer1 <wille.cervin@live.se>
2022-03-09 18:45:40 +0100

    ew

--- validate.js
+++ validate.js
@@ -2,9 +2,13 @@
 
 function validateinput(){
     // const quest1 = new question1("How many letters are there in 'Hello' ?", [1, 2, 5], 2);
-    alert("hello");
+    console.log("hey")
+    if(document.getElementById("firstname").value document.getElementById("lastname").value)
+    ;
+
+    
 }
 
 
-alert("hello")
+
 
