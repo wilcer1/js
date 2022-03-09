3b4b992570b7d06647c508c06d89da196ece0892
wilcer1 <79085590+wilcer1@users.noreply.github.com>
2022-03-09 10:12:37 +0100

    deleted shit

--- validate.js
+++ validate.js
@@ -8,22 +8,3 @@ function validateinput(){
 
 alert("hello")
 
-class question {
-    constructor(question, answers, right_answer_idx) {
-        this.question = question;
-        this.answers = answers;
-        this.right_answer_idx = right_answer_idx;
-    }
-  }
-class question1 extends question {
-    super();
-
-}
-// class question2 extends question {
-//     super()
-
-// }
-// class question3 extends question {
-//     super()
-
-// }
